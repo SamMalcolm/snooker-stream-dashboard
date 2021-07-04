e883075bfa602e515248e91f0f219f39e77d731b
SamMalcolm <sam_malcolm@live.com.au>
2021-07-05 09:55:05 +1000

    exploring ui

--- renderer.js
+++ renderer.js
@@ -4,3 +4,10 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+var directory = "";
+
+document.querySelector(".directory_selection").addEventListener("input", (e) => {
+	directory = e.target.value;
+	document.querySelector(".dir").innerHTML = "<i>Selected: " + directory + '</i>';
+})
